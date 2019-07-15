a73dd28cac2245b455218bf2806ac6e36b74af80
Boaz Mohar <boazmohar@gmail.com>
2019-07-15 09:44:46 -0400

    Added matlab analysis and flag, some prints for progress updates

--- pyUnBiased/RunIlastik.py
+++ pyUnBiased/RunIlastik.py
@@ -38,21 +38,23 @@ def run_one_object(all_args):
     return status, command, stdout.decode(), stderr.decode()
 
 
-def run_ilastik(directory, project_pixel=None, project_object=None):
+def run_ilastik(directory, project_pixel=None, project_object=None, matlab=None):
     """
 
     :param directory: raw files directory
     :param project_pixel: .ilp file for pixel classification
     :param project_object: .ilp file for object classification
+    :param matlab: flag for matlab mask extraction parallelization: 'files' or 'labels'
     :return: 1 if successful
     """
     # input checks
-    if project_pixel is None and project_object is None:
-        raise ValueError('Need at least one of project_pixel/project_object to be not None')
+    if project_pixel is None and project_object is None and matlab is None:
+        raise ValueError('Need at least one of project_pixel/project_object/matlab to be not None')
     directory_tiffs = os.path.join(directory, '*.tiff')
+    directory_tiffs = glob.glob(directory_tiffs)
+    directory_tiffs = [x for x in directory_tiffs if 'Cy5' not in x and 'FITC' not in x and 'Texas' not in x]
     if len(directory_tiffs) == 0:
         raise ValueError('found 0 *.tiff files in %s' % directory)
-
     # get system information
     cpu_count = multiprocessing.cpu_count()
     memory_mb = psutil.virtual_memory()[0] / 1000000
@@ -69,6 +71,7 @@ def run_ilastik(directory, project_pixel=None, project_object=None):
         program_string = '/groups/svoboda/home/moharb/ilastik-1.3.2post1-Linux/run_ilastik.sh --headless'
         project_string = '--project=/groups/svoboda/svobodalab/users/moharb/%s' % project_pixel
         os.system('{} {} {} {}'.format(env_string, program_string, project_string, directory_tiffs))
+        print('~~~~~~~~Done with pixel~~~~~~~~~~~~~~')
 
     if project_object is not None:
         # environment for object
@@ -79,13 +82,17 @@ def run_ilastik(directory, project_pixel=None, project_object=None):
         print('Object env: Found %d cpu_count giving 1, giving %dMB memory' % (cpu_count, mem_object))
 
         # prepare arguments
-        raw_files = natsorted(glob.glob(directory_tiffs))
+        directory_tiffs = os.path.join(directory, '*.tiff')
+        directory_tiffs = glob.glob(directory_tiffs)
+        directory_tiffs = [x for x in directory_tiffs if 'Cy5' not in x and 'FITC' not in x and 'Texas' not in x]
+        raw_files = natsorted(directory_tiffs)
         prob_files = natsorted(glob.glob(os.path.join(directory, '*.h5')))
         print('Found %d raw and %d prob files' % (len(raw_files), len(prob_files)))
         if not len(raw_files) == len(prob_files):
             raise ValueError('Number of files not the same %d != %d' % (len(raw_files), len(prob_files)))
         all_input_args = zip(raw_files, prob_files, repeat(project_object, len(raw_files)),
                              repeat(env_object, len(raw_files)))
+
 
         # run object
 
@@ -94,6 +101,13 @@ def run_ilastik(directory, project_pixel=None, project_object=None):
         all_status = sum([x[0] for x in object_results])
         if not len(raw_files) == all_status:
             raise ValueError('Some files failed object: %d != %d' % (len(raw_files), all_status))
+        print('~~~~~~~~Done with object~~~~~~~~~~~~~~')
+
+    if matlab:
+        os.chdir(directory)
+        os.system("/usr/local/matlab-2018b/bin/matlab -r 'try GetMasks_par_files_v2({});" 
+                  " catch; end; quit'".format(cpu_count))
+        print('~~~~~~~~Done with matlab~~~~~~~~~~~~~~')
     return 1
 
 
@@ -103,8 +117,9 @@ if __name__ == "__main__":
     parser.add_argument("directory", help="raw data directory")
     parser.add_argument("-p", "--project_pixel", help=".ilp file for pixel classification")
     parser.add_argument("-o", "--project_object", help=".ilp file for object classification")
+    parser.add_argument("-m", "--matlab", help="flag for matlab empty=don't run")
     args = parser.parse_args()
-    if not (args.project_pixel or args.project_object):
-        parser.error('No action requested, add -project_pixel or -project_object')
+    if not (args.project_pixel or args.project_object or args.matlab):
+        parser.error('No action requested, add project_pixel, project_object or matlab')
     run_ilastik(directory=args.directory, project_pixel=args.project_pixel,
-                project_object=args.project_object)
+                project_object=args.project_object, matlab=args.matlab)
